3ff9aa4ecc4c5bc751fc24834c2b976953b08d8f
Nico Bistolfi <nbistolfi@gmail.com>
2019-04-16 18:11:33 -0300

    Tests folder and class

---
+++ tests/EsetresTest.php
@@ -0,0 +1,23 @@
+<?php
+
+ /**
+  * @author    Nicolás Bistolfi <nbistolfi@gmail.com>
+  * @license   MIT
+  * @copyright 2019, Nicolás Bistolfi
+  *
+  * @link      http://github.com/dumpk/esetres
+  */
+ use PHPUnit\Framework\TestCase;
+
+ class PotracioTest extends TestCase
+ {
+     public function tearDown()
+     {
+     }
+
+     public function testPotracio()
+     {
+         // @TODO write a test
+         $this->assertTrue(true);
+     }
+ }
